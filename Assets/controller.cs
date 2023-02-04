a324fb8d44768b7684d50e46fbf27b81057ecd78
mokuzaru <kenny.otoya@pucp.pe>
2023-02-03 21:09:45 -0500

    first

---
+++ Assets/controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public CharacterController control;
+    public float vel = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        control.Move(mov * vel * Time.deltaTime);
+    }
+}
