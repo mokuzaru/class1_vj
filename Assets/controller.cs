d9118dedd8d71677fdddfbbb40157f863835626d
AlAstian68 <124413133+AlAstian68@users.noreply.github.com>
2023-02-03 21:54:57 -0500

    Update controller.cs

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -7,7 +7,12 @@ public class controller : MonoBehaviour
     public CharacterController control;
     public Rigidbody rb;
     public float vel = 5f;
-
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    
     // Start is called before the first frame update
     void Start()
     {
