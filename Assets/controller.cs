bf8ce1861327224f99046b0f66ceb0966cea650b
AlAstian68 <124413133+AlAstian68@users.noreply.github.com>
2023-02-03 22:02:15 -0500

    Update controller.cs

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -12,7 +12,7 @@ public class controller : MonoBehaviour
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +23,26 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        groundedPlayer = controller.isGrounded;
+        
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
         Vector3 mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        control.Move(move * Time.deltaTime * playerSpeed);
+        
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+        
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
 
+        playerVelocity.y += gravityValue * Time.deltaTime;
         control.Move(mov * vel * Time.deltaTime);
     }
 }
